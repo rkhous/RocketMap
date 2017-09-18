1b6b9241b2cb43fa5055ff3134677b3f6d23ba85
Michael Krug <michi.krug@gmail.com>
2017-09-18 22:05:58 +0200

    make serviceWorker.min.js path relative (#2291)

--- static/js/map.js
+++ static/js/map.js
@@ -2379,6 +2379,7 @@ $(function () {
     /* If push.js is unsupported or disabled, fall back to toastr
      * notifications. */
     Push.config({
+        serviceWorker: 'serviceWorker.min.js',
         fallback: function (notification) {
             sendToastrPokemonNotification(
                 notification.title,
