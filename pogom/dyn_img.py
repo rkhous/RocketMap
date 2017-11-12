a5df08eb7136d941f2ade89cf9896ec494193a58
sLoPPydrive <sloppydrive@gmail.com>
2017-11-12 01:28:36 +0100

    Cleaned up on-demand gym image creation code and added more battle indicator alternatives

--- pogom/dyn_img.py
+++ pogom/dyn_img.py
@@ -15,12 +15,77 @@ path_raid = os.path.join(path_images, 'raid')
 path_generated = os.path.join(path_images, 'generated')
 
 egg_images = {
-    1: 'egg_normal.png',
-    2: 'egg_normal.png',
-    3: 'egg_rare.png',
-    4: 'egg_rare.png',
-    5: 'egg_legendary.png'
+    1: os.path.join(path_raid, 'egg_normal.png'),
+    2: os.path.join(path_raid, 'egg_normal.png'),
+    3: os.path.join(path_raid, 'egg_rare.png'),
+    4: os.path.join(path_raid, 'egg_rare.png'),
+    5: os.path.join(path_raid, 'egg_legendary.png')
 }
+
+icon_size = 96
+badge_radius = 15
+padding = 1
+
+badge_upper_left = (padding + badge_radius, padding + badge_radius)
+badge_upper_right = (icon_size - (padding + badge_radius), padding + badge_radius)
+badge_lower_left = (padding + badge_radius, icon_size - (padding + badge_radius))
+badge_lower_right = (icon_size - (padding + badge_radius), icon_size - (padding + badge_radius))
+
+font = os.path.join('static', 'Arial Black.ttf')
+font_pointsize = 25
+
+
+def draw_raid_pokemon(pkm):
+    return draw_subject(os.path.join(path_icons, '{}.png'.format(pkm)), 64)
+
+
+def draw_raid_egg(raidlevel):
+    return draw_subject(egg_images[raidlevel], 36, 'center')
+
+
+def draw_gym_level(level):
+    return draw_badge(badge_lower_right, "black", "white", level)
+
+
+def draw_raid_level(raidlevel):
+    return draw_badge(badge_upper_right, "white", "black", raidlevel)
+
+
+def draw_battle_indicator():
+    return battle_indicator_swords()
+
+
+def battle_indicator_boom():
+    # BOOM! Sticker
+    return ['-gravity center ( {} -resize 84x84 ) -geometry +0+0 -composite'.format(
+        os.path.join(path_gym, 'boom.png'))]
+
+
+def battle_indicator_fist():
+    # Fist Badge
+    x = icon_size - (padding + badge_radius)
+    y = icon_size / 2
+    return [
+        '-fill white -stroke black -draw "circle {},{} {},{}"'.format(x, y, x - badge_radius, y),
+        '-gravity east ( {} -resize 24x24 ) -geometry +4+0 -composite'.format(os.path.join(path_gym, 'fist.png'))
+    ]
+
+
+def battle_indicator_flame():
+    # Flame Badge
+    return [
+        '-gravity east ( {} -resize 32x32 ) -geometry +0+0 -composite'.format(os.path.join(path_gym, 'flame.png'))
+    ]
+
+
+def battle_indicator_swords():
+    # Swords Badge
+    x = icon_size - (padding + badge_radius)
+    y = icon_size / 2
+    return [
+        '-fill white -stroke black -draw "circle {},{} {},{}"'.format(x, y, x - badge_radius, y),
+        '-gravity east ( {} -resize 24x24 ) -geometry +4+0 -composite'.format(os.path.join(path_gym, 'swords.png'))
+    ]
 
 
 def get_gym_icon(team, level, raidlevel, pkm, is_in_battle):
@@ -31,43 +96,39 @@ def get_gym_icon(team, level, raidlevel, pkm, is_in_battle):
     if not args.generate_images:
         return default_gym_image(team, level, raidlevel, pkm)
 
-    subject_lines = []
-    badge_lines = []
+    im_lines = []
     if pkm and pkm != 'null':
         # Gym with ongoing raid
         out_filename = os.path.join(path_generated, "{}_L{}_R{}_P{}.png".format(team, level, raidlevel, pkm))
-        subject_lines = draw_subject(os.path.join(path_icons, '{}.png'.format(pkm)), 64)
-        badge_lines.extend(draw_badge(80, 15, 15, "white", "black", raidlevel))
+        im_lines.extend(draw_raid_pokemon(pkm))
+        im_lines.extend(draw_raid_level(raidlevel))
         if level > 0:
-            badge_lines.extend(draw_badge(80, 80, 15, "black", "white", level))
+            im_lines.extend(draw_gym_level(level))
     elif raidlevel:
         # Gym with upcoming raid (egg)
         raidlevel = int(raidlevel)
         out_filename = os.path.join(path_generated, "{}_L{}_R{}.png".format(team, level, raidlevel))
-        subject_lines = draw_subject(os.path.join(path_raid, egg_images[raidlevel]), 36)
-        badge_lines.extend(draw_badge(80, 15, 15, "white", "black", raidlevel))
+        im_lines.extend(draw_raid_egg(raidlevel))
+        im_lines.extend(draw_raid_level(raidlevel))
         if level > 0:
-            badge_lines.extend(draw_badge(80, 80, 15, "black", "white", level))
+            im_lines.extend(draw_gym_level(level))
     elif level > 0:
         # Occupied gym
         out_filename = os.path.join(path_generated, '{}_L{}.png'.format(team, level))
-        badge_lines.extend(draw_badge(80, 80, 15, "black", "white", level))
+        im_lines.extend(draw_gym_level(level))
     else:
         # Neutral gym
         return os.path.join(path_gym, '{}.png'.format(team))
 
-    # Battle Badge
+    # Battle Indicator
     if is_in_battle:
-        subject_lines.append('-gravity center ( {} -resize 84x84 ) -geometry +0+0 -composite'.format(
-            os.path.join(path_gym, 'boom.png')))
         out_filename = out_filename.replace('.png', '_B.png')
+        im_lines.extend(draw_battle_indicator())
 
     if not os.path.isfile(out_filename):
         gym_image = os.path.join('static', 'images', 'gym', '{}.png'.format(team))
-        font = os.path.join('static', 'Arial Black.ttf')
-        cmd = 'convert {} {} -gravity center -font "{}" -pointsize 25 {} {}'.format(gym_image, join(subject_lines),
-                                                                                    font, join(badge_lines),
-                                                                                    out_filename)
+        cmd = 'convert {} -font "{}" -pointsize 25 {} {}'.format(gym_image, font, join(im_lines),
+                                                                 out_filename)
         if os.name != 'nt':
             cmd = cmd.replace(" ( ", " \( ").replace(" ) ", " \) ")
         subprocess.call(cmd, shell=True)
@@ -82,10 +143,11 @@ def draw_subject(image, size, gravity='north'):
     return lines
 
 
-def draw_badge(x, y, r, fill_col, text_col, text):
+def draw_badge(pos, fill_col, text_col, text):
+    (x, y) = pos
     lines = []
-    lines.append('-fill {} -stroke black -draw "circle {},{} {},{}"'.format(fill_col, x, y, x + r, y))
-    lines.append('-fill {} -draw "text {},{} \'{}\'"'.format(text_col, x - 48, y - 49, text))
+    lines.append('-fill {} -stroke black -draw "circle {},{} {},{}"'.format(fill_col, x, y, x + badge_radius, y))
+    lines.append('-gravity center -fill {} -stroke none -draw "text {},{} \'{}\'"'.format(text_col, x - 48, y - 49, text))
     return lines
 
 
